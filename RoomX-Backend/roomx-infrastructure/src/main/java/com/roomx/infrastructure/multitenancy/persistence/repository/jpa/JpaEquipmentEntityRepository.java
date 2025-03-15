53d3748f0ca4f9d5898459c9ed8691c2866be670
JiaG293 <giau32002@gmail.com>
2025-03-15 13:41:58 +0700

    chore: add jpa specification

--- RoomX-Backend/roomx-infrastructure/src/main/java/com/roomx/infrastructure/multitenancy/persistence/repository/jpa/JpaEquipmentEntityRepository.java
+++ RoomX-Backend/roomx-infrastructure/src/main/java/com/roomx/infrastructure/multitenancy/persistence/repository/jpa/JpaEquipmentEntityRepository.java
@@ -1,21 +1,20 @@
 package com.roomx.infrastructure.multitenancy.persistence.repository.jpa;
 
 import com.roomx.infrastructure.multitenancy.persistence.model.entity.EquipmentEntity;
-import com.roomx.infrastructure.multitenancy.persistence.model.entity.ServiceEntity;
-import io.micrometer.observation.ObservationFilter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface JpaEquipmentEntityRepository extends JpaRepository<EquipmentEntity, UUID> {
+public interface JpaEquipmentEntityRepository extends JpaRepository<EquipmentEntity, UUID>, JpaSpecificationExecutor<EquipmentEntity> {
 
     Optional<EquipmentEntity> findByEquipmentCode(String equipmentCode);
 
-    Page<EquipmentEntity> findAll(Specification<ServiceEntity> spec, Pageable pageable);
+    Page<EquipmentEntity> findAll(Specification<EquipmentEntity> spec, Pageable pageable);
 }
