0f3088e85ec26e5ead41dcd512b283adbc9a5e7f
SangHynh <huynhvansang.020102@gmail.com>
2025-05-27 21:23:46 +0700

    fix: public key env roi do hack di

--- dev-fe-web/src/firebase/firebase-config.ts
+++ dev-fe-web/src/firebase/firebase-config.ts
@@ -3,12 +3,12 @@ import { getMessaging, Messaging } from "firebase/messaging";
 
 // TypeScript sẽ check kỹ nên bạn cần đảm bảo các biến môi trường đều tồn tại
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN as string,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID as string,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET as string,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID as string,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID as string,
+  apiKey: 'AIzaSyC9tDqum9l-vDpj4Wh9_H1VzzYqS5dRV3w',
+  authDomain: 'roomx-61d9a.firebaseapp.com',
+  projectId: 'roomx-61d9a',
+  storageBucket: 'roomx-61d9a.firebasestorage.app',
+  messagingSenderId: '1029781988276',
+  appId: '1:1029781988276:web:a9a0df0c3f1a069c6f88e1',
 };
 
 // Initialize Firebase
